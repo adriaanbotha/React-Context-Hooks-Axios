3a1a0b4c1476d5dfeb3808bb1513234ebb2e2ea7
Adriaan Botha <adriaan.botha@gmail.com>
2019-05-24 10:23:58 +1000

    Making it more readable

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       <h1>React Hooks In Context</h1>
       <h2>Today's Fruit is {fruit}</h2>
       <Vegetable />
-      <h3>Vegies</h3>
+      <h3>Vegies and then News once the API Call returns</h3>
 
       {JSON.stringify(vegetable)}
 
